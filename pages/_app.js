2180f2dfc8bcfa8280ae759d92170951b4e09d93
thanhna <thanhna@ltsgroup.tech>
2024-10-03 12:02:00 +0700

    Initialize

---
+++ pages/_app.js
@@ -0,0 +1,16 @@
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import '../styles/globals.css';
+import { NextUIProvider } from "@nextui-org/react"
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <NextUIProvider>
+        <Header />
+        <Component {...pageProps} />
+        <Footer />
+      </NextUIProvider>
+    </>
+  );
+}
